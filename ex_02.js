bca2c9cad6acef5ab97fa22ecc2c102d98845369
karell606 <karell.azabe@epitech.digital>
2024-10-14 10:37:13 +0200

    ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,12 @@
+function checkFuel(fuel) {
+    return new Promise((resolve, reject) => {
+        if (fuel >= 9) {
+            resolve("Ready to launch");
+        } else {
+            reject("Please refill fuel");
+        }
+    });
+}
+checkFuel(9)
+    .then(message => console.log(message))
+    .catch(error => console.log(error));
